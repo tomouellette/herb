defe910f9e927592e35749a4252eadffeb73f665
Tom Ouellette <t.ouellette@mail.utoronto.ca>
2024-07-12 00:14:13 -0400

    Updating

--- backbones/zoo.py
+++ backbones/zoo.py
@@ -11,40 +11,40 @@ def select_backbone(
     kwargs = {"image_size": image_size, "channels": channels, **kwargs}
 
     if model == "vit_tiny":
-        from vit import vit_tiny
+        from backbones.vit import vit_tiny
         return vit_tiny(**kwargs)
     elif model == "vit_small":
-        from vit import vit_small
+        from backbones.vit import vit_small
         return vit_small(**kwargs)
     elif model == "vit_base":
-        from vit import vit_base
+        from backbones.vit import vit_base
         return vit_base(**kwargs)
     elif model == "vit_large":
-        from vit import vit_large
+        from backbones.vit import vit_large
         return vit_large(**kwargs)
     elif model == "mlp_mixer_tiny":
         from backbones.mlp_mixer import mlp_mixer_tiny
         return mlp_mixer_tiny(**kwargs)
     elif model == "mlp_mixer_small":
-        from mlp_mixer import mlp_mixer_small
+        from backbones.mlp_mixer import mlp_mixer_small
         return mlp_mixer_small(**kwargs)
     elif model == "mlp_mixer_base":
-        from mlp_mixer import mlp_mixer_base
+        from backbones.mlp_mixer import mlp_mixer_base
         return mlp_mixer_base(**kwargs)
     elif model == "mlp_mixer_large":
-        from mlp_mixer import mlp_mixer_large
+        from backbones.mlp_mixer import mlp_mixer_large
         return mlp_mixer_large(**kwargs)
     elif model == "navit_tiny":
-        from navit import navit_tiny
+        from backbones.navit import navit_tiny
         return navit_tiny(**kwargs)
     elif model == "navit_small":
-        from navit import navit_small
+        from backbones.navit import navit_small
         return navit_small(**kwargs)
     elif model == "navit_base":
-        from navit import navit_base
+        from backbones.navit import navit_base
         return navit_base(**kwargs)
     elif model == "navit_large":
-        from navit import navit_large
+        from backbones.navit import navit_large
         return navit_large(**kwargs)
     else:
         raise ValueError(f"Unknown model: {model}")
@@ -72,7 +72,7 @@ if __name__ == "__main__":
 
     for model in models:
         print(f"{prefix} Loading {model}")
-        model = select_backbone(model)
+        model = select_backbone(model, image_size=224, channels=3)
         x = torch.randn(1, 3, 224, 224)
         out = model(x)
         print(f"{prefix} {model.__class__.__name__}")
